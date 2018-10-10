40d4dcebd6dec39523b75da0373c808d738e328b
kerrymaths <kerry@digital-labs.co.uk>
2018-10-10 10:52:53 +0100

    fresh init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby wordpress test',
-    subtitle: 'Data fetched from wordpress',
+    title: 'Gatsby Starter (with cms background)',
   },
   plugins: [
-    // https://public-api.wordpress.com/wp/v2/sites/gatsbyjsexamplewordpress.wordpress.com/pages/
-    /*
-    * Gatsby's data processing layer begins with “source”
-    * plugins. Here the site sources its data from Wordpress.
-    */
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-wordpress`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        /*
-       * The base URL of the Wordpress site without the trailing slash and the protocol. This is required.
-       * Example : 'gatsbyjswpexample.wordpress.com' or 'www.example-site.com'
-       */
-        baseUrl: `http://localhost/gatsbywp/`,
-        // The protocol. This can be http or https.
-        protocol: `http`,
-        // Indicates whether the site is hosted on wordpress.com.
-        // If false, then the asumption is made that the site is self hosted.
-        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
-        // If your site is hosted on wordpress.org, then set this to false.
-        hostingWPCOM: false,
-        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
-        // This feature is untested for sites hosted on Wordpress.com
-        useACF: false,
-        // verboseOutput: true,
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
+    'gatsby-plugin-offline',
   ],
 }
